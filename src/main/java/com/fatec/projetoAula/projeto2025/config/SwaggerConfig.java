a86c7738981bea97431021453c1c04f932782fd6
Kevin Rezende <kevinrezende200@gmail.com>
2025-03-19 19:34:22 -0300

    Aula dia 19-03

---
+++ src/main/java/com/fatec/projetoAula/projeto2025/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.fatec.projetoAula.projeto2025.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+            .info(new Info()
+                .title("API de Projeto 2025")
+                .version("1.0")
+                .description("Documentação da API"));
+    }
+}
+
+
+
