12329e5374fa72aa096c7bf8b338933ee4cac8d4
Kevin Rezende <kevinrezende200@gmail.com>
2025-04-02 20:02:11 -0300

    Usuario Service Update

--- src/main/java/com/fatec/projetoAula/projeto2025/domain/usuario/UsuarioService.java
+++ src/main/java/com/fatec/projetoAula/projeto2025/domain/usuario/UsuarioService.java
@@ -1,5 +1,48 @@
 package com.fatec.projetoAula.projeto2025.domain.usuario;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fatec.projetoAula.projeto2025.entities.Usuario;
+
+@Service
 public class UsuarioService {
-    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario criarUsuario(Usuario usuario) {
+        usuario.setId(null);
+        return usuarioRepository.save(usuario);
+    }
+
+    public boolean atualizarUsuario(Integer id, Usuario usuarioAtualizado) {
+        Optional<Usuario> usuarioOptional = buscarUsuarioPorId(id);
+        if (usuarioOptional.isPresent()) {
+            Usuario usuario = usuarioOptional.get();
+            usuario.setNome(usuarioAtualizado.getNome());
+            // Adicione outros campos que precisam ser atualizados
+            usuarioRepository.save(usuario);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deletarUsuario(Integer id) {
+        if (usuarioRepository.existsById(id)) {
+            usuarioRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public Optional<Usuario> buscarUsuarioPorId(Integer id) {
+        return usuarioRepository.findById(id);
+    }
 }
